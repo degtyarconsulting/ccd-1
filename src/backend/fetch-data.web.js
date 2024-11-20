b7b437646dd897ebef19ad85668373e8b29a89c4
matthewdegtyar <mattdegtyar@gmail.com>
2024-11-20 17:29:03 -0500

    2024-11-20:1

---
+++ src/backend/fetch-data.web.js
@@ -0,0 +1,54 @@
+// backend/fetch-data.web.js
+
+import { Permissions, webMethod } from "wix-web-module";
+import { fetch } from "wix-fetch";
+import { getSecret } from "wix-secrets-backend";
+
+const getSecretKey = webMethod(Permissions.Anyone, async () => {
+  if (process.env.LOCAL_DEV) {
+    return 'flowhub-api-key';
+}
+  const key = await getSecret("flowhub-api-key");
+  return key
+});
+
+const getClientId = webMethod(Permissions.Anyone, async () => {
+  if (process.env.LOCAL_DEV) {
+    return 'flowhub-client-id';
+}
+  const id = await getSecret("flowhub-client-id");
+  return id
+});
+
+export const fetchAllProductData = webMethod(Permissions.Anyone, async () => {
+  const key = await getSecretKey();
+  const id = await getClientId();
+  const url = "https://api.flowhub.co/v0/inventory";
+
+  return fetch(url, {
+      method: "GET",
+      headers: {
+          "Accept": "application/json",
+          "clientId": id,
+          "key": key
+      }
+  })
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+  })
+  .catch((error) => {
+      console.error("Error fetching product data:", error);
+      throw error;
+  });
+});
+
+/**
+ * curl --request GET \
+ --url https://api.flowhub.co/v0/locations/3ca755c5-23b2-428d-b7e1-41b9cc9ce12c/inventory \
+ --header 'Accept: application/json' \
+ --header 'clientId: 310ef273-c68f-4caf-bbe5-c5aa3e3d0be1' \
+ --header 'key: 2ef703a4-a97e-4b6e-b1e8-d20c3dd78795'
+ */
---
+++ src/pages/404 Error Page.i66p3.js
@@ -0,0 +1,14 @@
+// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import { fetchAllProductData } from "backend/fetch-data.web.js";
+
+async function init() {
+  const cannabisData = await fetchAllProductData();
+  // Send cannabis data to the iframe
+  $w("#html1").postMessage(cannabisData); // Use '*' to allow any origin or specify a target origin for security
+
+}
+
+$w.onReady(function () {
+  init();
+});
